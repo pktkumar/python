209bd0ade20b89891d20b35cc60f05020da0b420
Kumar Vaduganathan <kumarvaduganathan@MacBookPro.lan>
2025-01-05 14:34:26 -0600

    Update pythonSlice.py

--- pythonSlice.py
+++ pythonSlice.py
@@ -1,3 +1,26 @@
 String = 'Hello_World'
 slice_obj = slice(5,11)
 print(String[slice_obj]) #this will print _World
+
+# String slicing
+String = '0123456789'
+slice_obj1 = slice(3)
+slice_obj2 = slice(0, 5)
+slice_obj3 = slice(0, 5, 3)
+
+print("String slicing")
+print(String[slice_obj1])
+print(String[slice_obj2])
+print(String[slice_obj3])
+
+
+# String slicing
+String = '0123456789'
+slice_obj1 = slice(2)
+slice_obj2 = slice(1, 7)
+slice_obj3 = slice(1, 8, 3)
+
+print("String slicing")
+print(String[slice_obj1])
+print(String[slice_obj2])
+print(String[slice_obj3])
